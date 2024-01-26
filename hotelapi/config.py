36660b418796669e3e2f9f2b3ef0f3b12c3f01cf
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-01-26 20:57:39 +0100

    Adds hotelapi
    * implements min. __init__ setup
    * test /hello with curl, all good

---
+++ hotelapi/config.py
@@ -0,0 +1,13 @@
+import os
+SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = True
+
+# Connect to the database
+# TODO IMPLEMENT DATABASE URL
+username = os.environ.get('USER', os.environ.get('USERNAME'))
+SQLALCHEMY_DATABASE_URI = 'postgresql://{}@localhost:5432/hotelapi'.format(username)
+SQLALCHEMY_TRACK_MODIFICATIONS = False

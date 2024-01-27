611fce6573b12127825bf4e68315a80948bea56b
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-01-27 12:31:52 +0100

    Implements getReservation by id

--- hotelapi/__init__.py
+++ hotelapi/__init__.py
@@ -23,30 +23,40 @@ def create_app(test_config=None):
 
     migrate = Migrate(app, db)
 
-    # a simple page that says hello
     @app.route('/reservations')
     @cross_origin()
     def getReservations():
         reservation = ReservationData()
         reservation.id = 1
-        reservation.checkInDate = str(datetime.now())
-        reservation.checkOutDate = str(datetime.now())
+        reservation.checkInDate = '2024-01-27'
+        reservation.checkOutDate = '2024-01-28'
         reservation.guestName = 'Test'
         reservation.guestEmail = 'test@email.com'
         reservation.roomNumber = 1
 
         reservation2 = ReservationData()
         reservation2.id = 2
-        reservation2.checkInDate = str(datetime.now())
-        reservation2.checkOutDate = str(datetime.now())
+        reservation2.checkInDate = '2024-01-30'
+        reservation2.checkOutDate = '2024-02-02'
         reservation2.guestName = 'Test2'
         reservation2.guestEmail = 'test2@email.com'
         reservation2.roomNumber = 2
 
+        reservations = '[{}]'.format(reservation.toJSON() + ', ' + reservation2.toJSON())
+        return reservations
 
-        
+    @app.route('/reservation/<int:id>')
+    @cross_origin()
+    def getReservation(id: int):
 
-        users = '[{}]'.format(reservation.toJSON() + ', ' + reservation2.toJSON())
-        return users
+        reservation = ReservationData()
+        reservation.id = 1
+        reservation.checkInDate = '2024-01-27'
+        reservation.checkOutDate = '2024-01-27'
+        reservation.guestName = 'Test'
+        reservation.guestEmail = 'test@email.com'
+        reservation.roomNumber = 1
+
+        return reservation.toJSON()
 
     return app

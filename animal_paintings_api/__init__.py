0acc092d0c556d60a237e2f4c1c8b017b7f62066
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-01-30 19:22:58 +0100

    Implements getImage endpoint

--- animal_paintings_api/__init__.py
+++ animal_paintings_api/__init__.py
@@ -1,6 +1,8 @@
 from flask import (
     Flask,
     jsonify,
+    send_file,
+    abort
     )
 from flask_cors import (
     CORS,
@@ -8,6 +10,9 @@ from flask_cors import (
     )
 import json
 from animal_paintings_api.models.product import Product
+from pathlib import Path
+
+import sys
 
 def create_app(test_config=None):
 
@@ -23,9 +28,24 @@ def create_app(test_config=None):
         image_url= str(d.get('image_url')),
         ))
 
+
     @app.route('/products')
     @cross_origin()
     def getProducts():
         return jsonify(data)
+
+
+    @app.route('/image/<string:id>')
+    @cross_origin()
+    def getImage(id: str):
+        
+        file_name = 'resources/images/{}'.format(id)
+
+        image_file = Path('./animal_paintings_api/{}'.format(file_name))
+        if image_file.is_file():
+            return send_file(file_name, mimetype='image/jpeg')
+        else:
+            abort(404)
+
 
     return app

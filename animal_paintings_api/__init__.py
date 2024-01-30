e495dd12906b8a8d30a808e1ff0554baee9bcf98
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-01-30 16:55:32 +0100

    Fixes data.json to serialize correct response body

--- animal_paintings_api/__init__.py
+++ animal_paintings_api/__init__.py
@@ -7,7 +7,7 @@ from flask_cors import (
     cross_origin,
     )
 import json
-
+from animal_paintings_api.models.product import Product
 
 def create_app(test_config=None):
 
@@ -15,9 +15,13 @@ def create_app(test_config=None):
 
     cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-    file = open('./animal-paintings-api/resources/data.json')
-    data = json.load(file)
-
+    file = open('./animal_paintings_api/resources/data.json')
+    data = json.load(file, object_hook = lambda d : Product(
+        id= str(d.get('id')),
+        name= str(d.get('name')),
+        price= str(d.get('price')),
+        image_url= str(d.get('image_url')),
+        ))
 
     @app.route('/products')
     @cross_origin()

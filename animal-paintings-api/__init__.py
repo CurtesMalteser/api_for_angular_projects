a497cd5e19bfe551af2649fd52992b3e52e1ce48
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-01-30 15:44:34 +0100

    Declares GET /products
    * Imports Amazing Animal Paintings data from course
    * test via curl

--- animal-paintings-api/__init__.py
+++ animal-paintings-api/__init__.py
@@ -1,8 +1,27 @@
-from flask import Flask
+from flask import (
+    Flask,
+    jsonify,
+    )
+from flask_cors import (
+    CORS,
+    cross_origin,
+    )
+import json
 
 
 def create_app(test_config=None):
 
     app = Flask(__name__, instance_relative_config=True)
+
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+    file = open('./animal-paintings-api/resources/data.json')
+    data = json.load(file)
+
+
+    @app.route('/products')
+    @cross_origin()
+    def getProducts():
+        return jsonify(data)
 
     return app

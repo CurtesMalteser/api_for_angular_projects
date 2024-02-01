ddbe43b525aaf2d23708065b8f92d407b7992460
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-02-01 16:34:51 +0100

    Implements GET /success and /failure to mock success error cases

--- book_managment_api/__init__.py
+++ book_managment_api/__init__.py
@@ -13,6 +13,8 @@ from book_managment_api.models.book import Book
 
 books : list[Book] = []
 
+is_success : bool = True
+
 def create_app(test_config=None):
 
     app = Flask(__name__, instance_relative_config=True)
@@ -22,25 +24,46 @@ def create_app(test_config=None):
     @app.route('/books')
     @cross_origin()
     def getBooks():
-        return jsonify(books)
+        if(is_success):
+            return jsonify(books)
+        else:
+            abort(404, "Mocked failure! Call GET /success.")
 
     @app.route('/book', methods=['POST'])
     @cross_origin()
     def addBook():
-        content_type = request.headers.get('Content-Type')
-        if ('application/json' in str(content_type)):
-            try:
-                json_data = json.dumps(request.json)
-                book = json.loads(json_data, object_hook = lambda d : Book.fromDict(d = d))
+        if(is_success):
+            content_type = request.headers.get('Content-Type')
+            if ('application/json' in str(content_type)):
+                try:
+                    json_data = json.dumps(request.json)
+                    book = json.loads(json_data, object_hook = lambda d : Book.fromDict(d = d))
 
-                books.append(book)
+                    books.append(book)
 
-                return jsonify(request.json)
+                    return jsonify(request.json)
 
-            except:
-                abort(422, "JSON malformed.")
-        
+                except:
+                    abort(422, "JSON malformed.")
+
+            else:
+                abort(404, "Content type is not supported.")
         else:
-            abort(404, "Content type is not supported.")
+            abort(404, "Mocked failure! Call GET /success.")
+
+
+    @app.route('/success')
+    @cross_origin()
+    def isSuccess():
+        global is_success
+        is_success = True
+        return jsonify("isSuccess: True")
+
+    @app.route('/failure')
+    @cross_origin()
+    def isFailure():
+        global is_success
+        is_success = False
+        return jsonify("isSuccess: False")
 
     return app

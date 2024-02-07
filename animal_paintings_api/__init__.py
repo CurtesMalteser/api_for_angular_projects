a22ca5ecfe544b47d93a2661490a68e26d0bebd9
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-02-07 19:39:14 +0100

    Implements after request headers and methods

--- animal_paintings_api/__init__.py
+++ animal_paintings_api/__init__.py
@@ -19,11 +19,17 @@ def create_app(test_config=None):
 
     app = Flask(__name__, instance_relative_config=True)
 
-    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+    cors = CORS(app, resources={r"*/api/*": {"origins": "*"}})
 
     file = open('./animal_paintings_api/resources/data.json')
     data = json.load(file, object_hook = lambda d : Product.fromDict(d = d))
 
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
 
     @app.route('/products')
     @cross_origin()
--- book_managment_api/__init__.py
+++ book_managment_api/__init__.py
@@ -19,7 +19,13 @@ def create_app(test_config=None):
 
     app = Flask(__name__, instance_relative_config=True)
 
-    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+    cors = CORS(app, resources={r"*/api/*": {"origins": "*"}})
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
 
     @app.route('/books')
     @cross_origin()
